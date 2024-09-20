13136c108477cd7a4b898d181ec1f28a50b2098c
liuqianhong <liuqianhong@outlook.com>
2024-09-21 00:22:43 +0800

    更改删除图标

--- static/script.js
+++ static/script.js
@@ -13,9 +13,7 @@ const ZOOM_SPEED = 0.1;
 const MAX_SCALE = 3;
 const MIN_SCALE = 0.5;
 
-const trashBinIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 016.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-</svg>`;
+const trashBinIcon = `<i class="far fa-trash-alt"></i>`;
 
 // 检查 translations 对象
 if (typeof translations === 'undefined') {
